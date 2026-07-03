b9453b3077f798ed859a3fb5ccafd8fb3e7f5afb
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-03 08:24:48 -0400

    Bump SW to v31 — force CDN invalidation

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v30';
+const CACHE = 'groomers-v31';
 
 self.addEventListener('install', () => self.skipWaiting());
 
