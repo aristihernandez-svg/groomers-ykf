330ceb103cc7c2ad20363eeed26bd391aaf39153
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-03 09:19:26 -0400

    Add debug raw fetch button in Migration Manager; bump SW v38

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v37';
+const CACHE = 'groomers-v38';
 
 self.addEventListener('install', () => self.skipWaiting());
 
