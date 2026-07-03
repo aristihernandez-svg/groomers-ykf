a32296a5763f51527741dcabc74d2f89fa455e64
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-03 08:05:47 -0400

    Bump SW cache to v30 — force GitHub Pages redeploy

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v29';
+const CACHE = 'groomers-v30';
 
 self.addEventListener('install', () => self.skipWaiting());
 
