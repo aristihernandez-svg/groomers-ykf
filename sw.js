9553993999bfd341b4f5ac2450d8f96e08fe33ed
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-03 09:06:02 -0400

    Debug: log non-task rows in fetchDayRaw; bump SW v36

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v35';
+const CACHE = 'groomers-v36';
 
 self.addEventListener('install', () => self.skipWaiting());
 
