986a6130c2c0c64f3da01a77e649f24c4355d9af
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-03 08:52:23 -0400

    Always show migration button; re-import from Firestore mode; bump SW v34

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v33';
+const CACHE = 'groomers-v34';
 
 self.addEventListener('install', () => self.skipWaiting());
 
