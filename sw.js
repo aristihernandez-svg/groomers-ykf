741fb81b0923769df9b8a147e2f5805c713501d2
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-04 08:35:10 -0400

    Fix URL auto-fetch on paste/change events; bump SW v46

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v45';
+const CACHE = 'groomers-v46';
 
 self.addEventListener('install', () => self.skipWaiting());
 
