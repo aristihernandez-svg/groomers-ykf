a91457a0d11ee623433cd79ec36081d72f489421
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-04 15:00:43 -0400

    Add fuzzy duplicate task detection; bump SW v52

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v51';
+const CACHE = 'groomers-v52';
 
 self.addEventListener('install', () => self.skipWaiting());
 
