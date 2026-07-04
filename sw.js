7f3b6fa9012fefb2cdbce98dc21982293e277009
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-04 15:22:47 -0400

    Bump SW v55

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v54';
+const CACHE = 'groomers-v55';
 
 self.addEventListener('install', () => self.skipWaiting());
 
