ece9eacb3a7209ded82dac1f607d7864b945dc40
aristihernandez-svg <aristihernandez@gmail.com>
2026-07-04 14:28:37 -0400

    Bump SW v49 to force CDN cache bust

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Groomers YKF — Service Worker v3
 // Strategy: network-first, wipe all old caches on activate
 
-const CACHE = 'groomers-v48';
+const CACHE = 'groomers-v49';
 
 self.addEventListener('install', () => self.skipWaiting());
 
